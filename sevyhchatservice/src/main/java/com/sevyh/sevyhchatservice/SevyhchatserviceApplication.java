6896b6353982a68efc375c8c2dc4bbfe080576df
Jules <jules.houben@live.nl>
2023-03-22 11:47:25 +0100

    Initialize maven project

---
+++ sevyhchatservice/src/main/java/com/sevyh/sevyhchatservice/SevyhchatserviceApplication.java
@@ -0,0 +1,13 @@
+package com.sevyh.sevyhchatservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SevyhchatserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SevyhchatserviceApplication.class, args);
+	}
+
+}
---
+++ sevyhchatservice/src/test/java/com/sevyh/sevyhchatservice/SevyhchatserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sevyh.sevyhchatservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SevyhchatserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
