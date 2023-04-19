7f18e181a76fea8ed1c4b81e243c75eb9015147a
Jules <jules.houben@live.nl>
2023-04-19 23:30:14 +0200

    Incomming messages from RabbitMQ are now saved to the database

---
+++ src/main/java/com/sevyh/sevyhchatservice/config/AppConfig.java
@@ -0,0 +1,25 @@
+package com.sevyh.sevyhchatservice.config;
+
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    @Primary
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        return mapper;
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter rabbitMessageConverter(ObjectMapper objectMapper) {
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
+}
