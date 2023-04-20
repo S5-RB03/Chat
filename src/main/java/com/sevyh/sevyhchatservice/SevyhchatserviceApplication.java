e328e3d973eff0a8b5a880fbc8de07a8a5ae401a
Jules Houben <jules.houben@live.nl>
2023-04-20 13:31:08 +0200

    Add cors settings

--- src/main/java/com/sevyh/sevyhchatservice/SevyhchatserviceApplication.java
+++ src/main/java/com/sevyh/sevyhchatservice/SevyhchatserviceApplication.java
@@ -3,6 +3,17 @@ package com.sevyh.sevyhchatservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+
+import java.util.Arrays;
+
+import java.util.List;
+
+
 @SpringBootApplication
 public class SevyhchatserviceApplication {
 
@@ -10,4 +21,15 @@ public class SevyhchatserviceApplication {
 		SpringApplication.run(SevyhchatserviceApplication.class, args);
 	}
 
+	@Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(
+        Arrays.asList("GET", "POST", "PUT","DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Content-Type"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
